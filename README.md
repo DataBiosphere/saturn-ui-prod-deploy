5250668c5327e76e7f44f950d96ea0d1ac30f980
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2018-07-11 14:35:02 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Saturn prod deploy
+
+Runs the Saturn production deploy process. This is done via scheduled workflow on CircleCI. See the `config.yml` file for details.
---
+++ src/find-build.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch')
+
+const BASE = 'https://circleci.com/api/v1.1/project/github/DataBiosphere/saturn-ui'
+
+const findBuild = async () => {
+  const jobs = await fetch(`${BASE}/tree/dev?filter=successful`).then(r => r.json())
+  const latest = jobs.find(j => j.workflows && j.workflows.job_name === 'build')
+  const artifacts = await fetch(`${BASE}/${latest.build_num}/artifacts`).then(r => r.json())
+  console.log(artifacts[0].url)
+}
+
+findBuild()
