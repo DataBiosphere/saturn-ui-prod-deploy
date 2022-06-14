182864b44ccbf9ffe09b8123f9097d4ec694c3c4
Nick Watts <1156625+nawatts@users.noreply.github.com>
2022-06-14 10:23:19 -0400

    Output deployed revision (#30)

--- src/find-build.js
+++ src/find-build.js
@@ -7,11 +7,18 @@ const findBuild = async () => {
   const workflows = await fetch(`${base}/insights/${projectSlug}/workflows/build-deploy?branch=dev`).then(r => r.json()).then(o => o.items)
   const latestWorkflow = workflows.find(w => w.status === 'success')
 
+  const workflow = await fetch(`${base}/workflow/${latestWorkflow.id}`).then(r => r.json())
+  const pipeline = await fetch(`${base}/pipeline/${workflow.pipeline_id}`).then(r => r.json())
+  const revision = pipeline.vcs.revision
+
   const workflowJobs = await fetch(`${base}/workflow/${latestWorkflow.id}/job`).then(r => r.json()).then(o => o.items)
   const buildJob = workflowJobs.find(j => j.name === 'build' && j.status === 'success')
 
   const jobArtifacts = await fetch(`${base}/project/${projectSlug}/${buildJob.job_number}/artifacts`).then(r => r.json()).then(o => o.items)
-  console.log(jobArtifacts[0].url)
+  const artifactUrl = jobArtifacts[0].url
+
+  const buildInfo = { artifactUrl, revision }
+  console.log(JSON.stringify(buildInfo, null, 2))
 }
 
 findBuild()
