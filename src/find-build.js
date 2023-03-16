eb76f02b7e72da746deab8800056e67ad39c510c
Nick Watts <nwatts@broadinstitute.org>
2023-03-16 13:01:37 -0400

    Retry CircleCI API requests

--- src/find-build.js
+++ src/find-build.js
@@ -4,18 +4,41 @@ const base = 'https://circleci.com/api/v2'
 const projectSlug = 'gh/DataBiosphere/terra-ui'
 const buildArtifactPath = 'build.tgz'
 
+const delay = t => new Promise(resolve => setTimeout(resolve, t * 1000))
+
+const fetchCircleCI = async (path, opts = {}) => {
+  const { maxRetries = 5 } = opts
+
+  let response = await fetch(`${base}/${path}`)
+  let nRequests = 1
+
+  while (!response.ok && nRequests < maxRetries) {
+    await delay(nRequests)
+    response = await fetch(`${base}/${path}`)
+    nRequests += 1
+  }
+
+  if (response.ok) {
+    const responseContent = await response.json()
+    return responseContent
+  } else {
+    const responseContent = await response.text()
+    throw new Error(`CircleCI request failed: ${responseContent}`)
+  }
+}
+
 const findBuild = async () => {
-  const workflows = await fetch(`${base}/insights/${projectSlug}/workflows/build-deploy?branch=dev`).then(r => r.json()).then(o => o.items)
+  const workflows = await fetchCircleCI(`insights/${projectSlug}/workflows/build-deploy?branch=dev`).then(o => o.items)
   const latestWorkflow = workflows.find(w => w.status === 'success')
 
-  const workflow = await fetch(`${base}/workflow/${latestWorkflow.id}`).then(r => r.json())
-  const pipeline = await fetch(`${base}/pipeline/${workflow.pipeline_id}`).then(r => r.json())
+  const workflow = await fetchCircleCI(`workflow/${latestWorkflow.id}`)
+  const pipeline = await fetchCircleCI(`pipeline/${workflow.pipeline_id}`)
   const revision = pipeline.vcs.revision
 
-  const workflowJobs = await fetch(`${base}/workflow/${latestWorkflow.id}/job`).then(r => r.json()).then(o => o.items)
+  const workflowJobs = await fetchCircleCI(`workflow/${latestWorkflow.id}/job`).then(o => o.items)
   const buildJob = workflowJobs.find(j => j.name === 'build' && j.status === 'success')
 
-  const jobArtifacts = await fetch(`${base}/project/${projectSlug}/${buildJob.job_number}/artifacts`).then(r => r.json()).then(o => o.items)
+  const jobArtifacts = await fetchCircleCI(`project/${projectSlug}/${buildJob.job_number}/artifacts`).then(o => o.items)
   const buildArtifact = jobArtifacts.find(artifact => artifact.path === buildArtifactPath)
   const artifactUrl = buildArtifact.url
 
