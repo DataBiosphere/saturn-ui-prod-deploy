52c4df5caf95c24c32272a76375ff01f7e1684d7
Nick Watts <nwatts@broadinstitute.org>
2023-03-17 11:14:33 -0400

    Set cookie for job artifacts request

--- src/find-build.js
+++ src/find-build.js
@@ -5,6 +5,11 @@ const projectSlug = 'gh/DataBiosphere/terra-ui'
 const buildArtifactPath = 'build.tgz'
 
 const findBuild = async () => {
+  // This request returns 401 but sets a cookie that we need for the job artifacts endpoint.
+  // Why only that endpoint needs the cookie is unknown.
+  const profileResponse = await fetch('https://circleci.com/api/v1.1/me')
+  const cookie = profileResponse.headers.raw()['set-cookie']
+
   const workflows = await fetch(`${base}/insights/${projectSlug}/workflows/build-deploy?branch=dev`).then(r => r.json()).then(o => o.items)
   const latestWorkflow = workflows.find(w => w.status === 'success')
 
@@ -15,7 +20,7 @@ const findBuild = async () => {
   const workflowJobs = await fetch(`${base}/workflow/${latestWorkflow.id}/job`).then(r => r.json()).then(o => o.items)
   const buildJob = workflowJobs.find(j => j.name === 'build' && j.status === 'success')
 
-  const jobArtifacts = await fetch(`${base}/project/${projectSlug}/${buildJob.job_number}/artifacts`).then(r => r.json()).then(o => o.items)
+  const jobArtifacts = await fetch(`${base}/project/${projectSlug}/${buildJob.job_number}/artifacts`, { headers: { cookie }}).then(r => r.json()).then(o => o.items)
   const buildArtifact = jobArtifacts.find(artifact => artifact.path === buildArtifactPath)
   const artifactUrl = buildArtifact.url
 
