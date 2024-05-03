a6645ca7e9fdb0bf7608c78eaf615abe94c6f3b3
Nick Watts <nwatts@broadinstitute.org>
2024-05-03 12:17:07 -0400

    Use older API to list job artifacts

--- src/find-build.js
+++ src/find-build.js
@@ -1,26 +1,21 @@
 const fetch = require('node-fetch')
 
-const base = 'https://circleci.com/api/v2'
+const base = 'https://circleci.com/api'
 const projectSlug = 'gh/DataBiosphere/terra-ui'
 const buildArtifactPath = 'build.tgz'
 
 const findBuild = async () => {
-  // This request returns 401 but sets a cookie that we need for the job artifacts endpoint.
-  // Why only that endpoint needs the cookie is unknown.
-  const profileResponse = await fetch('https://circleci.com/api/v1.1/me')
-  const cookie = profileResponse.headers.raw()['set-cookie']
-
-  const workflows = await fetch(`${base}/insights/${projectSlug}/workflows/build-deploy?branch=dev`).then(r => r.json()).then(o => o.items)
+  const workflows = await fetch(`${base}/v2/insights/${projectSlug}/workflows/build-deploy?branch=dev`).then(r => r.json()).then(o => o.items)
   const latestWorkflow = workflows.find(w => w.status === 'success')
 
-  const workflow = await fetch(`${base}/workflow/${latestWorkflow.id}`).then(r => r.json())
-  const pipeline = await fetch(`${base}/pipeline/${workflow.pipeline_id}`).then(r => r.json())
+  const workflow = await fetch(`${base}/v2/workflow/${latestWorkflow.id}`).then(r => r.json())
+  const pipeline = await fetch(`${base}/v2/pipeline/${workflow.pipeline_id}`).then(r => r.json())
   const revision = pipeline.vcs.revision
 
-  const workflowJobs = await fetch(`${base}/workflow/${latestWorkflow.id}/job`).then(r => r.json()).then(o => o.items)
+  const workflowJobs = await fetch(`${base}/v2/workflow/${latestWorkflow.id}/job`).then(r => r.json()).then(o => o.items)
   const buildJob = workflowJobs.find(j => j.name === 'build' && j.status === 'success')
 
-  const jobArtifacts = await fetch(`${base}/project/${projectSlug}/${buildJob.job_number}/artifacts`, { headers: { cookie }}).then(r => r.json()).then(o => o.items)
+  const jobArtifacts = await fetch(`${base}/v1.1/project/${projectSlug}/${buildJob.job_number}/artifacts`).then(r => r.json());
   const buildArtifact = jobArtifacts.find(artifact => artifact.path === buildArtifactPath)
   const artifactUrl = buildArtifact.url
 
